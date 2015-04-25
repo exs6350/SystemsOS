82bed2bb170ae94d6a501434bd65a43361d46509
Max Roth <mdrnotadoctor@gmail.com>
2015-04-25 12:48:19 -0400

    Adding files for file system

--- fileSystem.c
+++ fileSystem.c
@@ -6,4 +6,19 @@
 ** Description:	Implementation of Simple File System
 */
 
+#include "types.h"
 #include "fileSystem.h"
+
+/*
+** Create and initialize a file system, returning it's representative
+** sfs_file_table structure
+*/
+sfs_file_table* sfs_init( void ) {
+
+	/*sfs_file_table* filesys = (sfs_file_table*) \
+		malloc(sizeof(sfs_file_table)); //find malloc equivalent
+
+	//Find another way to do this, not neccesary but would be nice
+	memset(filesys, 0, sizeof(&filesys)); //Clean data is good data
+	filesys->current_location = -1;*/
+}
